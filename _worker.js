aa8ed00a30b160da2b6d0eebfc6aa0cc7c3d119a
CMLiussss <24787744+cmliu@users.noreply.github.com>
2024-03-13 22:09:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -21,44 +21,66 @@ export default {
 
 		// 从请求的 URL 中获取需要的参数
 		const url = new URL(request.url);
-
-		// 获取 URL 查询参数中的 'token'，如果不存在则赋值为 "null"
-		const token = url.searchParams.get('token') || "null";
+		let token;
+		if (url.pathname === `/${mytoken}`){
+			token = mytoken;
+		} else {
+			// 获取 URL 查询参数中的 'token'，如果不存在则赋值为 "null"
+			token = url.searchParams.get('token') || "null";
+		}
 
 		// 检查提供的 token 是否与 mytoken 相符
 		if (token === mytoken) {
 			const 文件名 = url.pathname;
-			// 获取 URL 查询参数中的 'text' 和 'b64'，如果不存在则赋值为 "null"
-			const text = url.searchParams.get('text') || "null";
-			const b64 = url.searchParams.get('b64') || "null";
 
-			// 如果 'text' 和 'b64' 都为 "null"，则从 KV 中读取并返回文件内容
-			if (text === "null" && b64 === "null"){
-				const value = await KV.get(文件名);
-				return new Response(value , {
-					status: 200,
-					headers: { 'content-type': 'text/plain; charset=utf-8' },
+			if (文件名 == "/config" || 文件名 == `/${mytoken}`) {
+				const html = configHTML(url.hostname, token);
+				return new Response(html, {
+				  headers: {
+					'Content-Type': 'text/html; charset=UTF-8',
+				  },
+				});
+			} else if (文件名 == "/config/update.bat") {
+				return new Response(下载bat(url.hostname, token), {
+				  headers: {
+					"Content-Disposition": `attachment; filename=update.bat`, 
+					"content-type": "text/plain; charset=utf-8",
+				  },
 				});
 			} else {
-				// 检查文件是否存在
-				await fileExists(KV, 文件名);
-				  
-				// 如果 'b64' 为 "null" ，则以明文方式写入文件，如果 'text' 为 "null" ，则以 base64 方式写入文件
-				if (b64 === "null" ){
-					await KV.put(文件名, text);
-					return new Response(text, {
+				// 获取 URL 查询参数中的 'text' 和 'b64'，如果不存在则赋值为 "null"
+				const text = url.searchParams.get('text') || "null";
+				const b64 = url.searchParams.get('b64') || "null";
+
+				// 如果 'text' 和 'b64' 都为 "null"，则从 KV 中读取并返回文件内容
+				if (text === "null" && b64 === "null"){
+					const value = await KV.get(文件名);
+					return new Response(value , {
 						status: 200,
 						headers: { 'content-type': 'text/plain; charset=utf-8' },
 					});
-				} else if (text === "null" ){
-					await KV.put(文件名, base64Decode(b64));
-					return new Response(base64Decode(b64), {
-						status: 200,
-						headers: { 'content-type': 'text/plain; charset=utf-8' },
-					});
+				} else {
+					// 检查文件是否存在
+					await fileExists(KV, 文件名);
+					
+					// 如果 'b64' 为 "null" ，则以明文方式写入文件，如果 'text' 为 "null" ，则以 base64 方式写入文件
+					if (b64 === "null" ){
+						await KV.put(文件名, text);
+						return new Response(text, {
+							status: 200,
+							headers: { 'content-type': 'text/plain; charset=utf-8' },
+						});
+					} else if (text === "null" ){
+						await KV.put(文件名, base64Decode(空格替换加号(b64)));
+						return new Response(base64Decode(空格替换加号(b64)), {
+							status: 200,
+							headers: { 'content-type': 'text/plain; charset=utf-8' },
+						});
+					}
 				}
 			}
-			// 如果 token 不符，返回 'token 有误'
+
+			// 如果 token 不符，返回 'token 有误'//
 		} else {
 			return new Response('token 有误', {
 				status: 400,
@@ -80,3 +102,61 @@ function base64Decode(str) {
 	const decoder = new TextDecoder('utf-8');
 	return decoder.decode(bytes);
 }
+
+function 空格替换加号(str) {
+	str = str.replace(/ /g, '+');
+	return str;
+}
+
+function 下载bat(域名,token) {
+	return `
+@echo off
+chcp 65001
+setlocal
+
+set "DOMAIN=${域名}"
+set "TOKEN=${token}"
+
+rem %~nx1表示第一个参数的文件名和扩展名
+set "FILENAME=%~nx1"
+
+rem PowerShell command to read the file and base64 encode the content
+for /f "delims=" %%i in ('powershell -command "[convert]::ToBase64String([io.file]::ReadAllBytes('%cd%\%FILENAME%'))"') do set "BASE64_TEXT=%%i"
+
+rem 将内容保存到response.txt
+rem echo %BASE64_TEXT% > response.txt
+
+rem 构造带有文件名和内容作为参数的URL
+set "URL=https://%DOMAIN%/%FILENAME%?token=%TOKEN%&b64=%BASE64_TEXT%"
+
+rem 显示请求的响应 
+powershell -Command "(Invoke-WebRequest -Uri '%URL%').Content"
+
+endlocal
+
+rem 执行完成后继续保持窗口打开
+pause
+
+`;
+}
+
+function configHTML(域名, token) {
+	return `
+	  <html>
+		<head>
+		  <title>CF-Workers-TEXT2KV</title>
+		</head>
+
+		<body>
+		  <h1 class="centered">CF-Workers-TEXT2KV 配置信息</h1>
+		  <p class="centered">
+		  服务域名: ${域名} <br>
+		  token: ${token} <br>
+		  <br>
+		  Windows <a href="https://${域名}/config/update.bat?token=${token}">Update.bat 下载</a><br>
+		  </p>
+	  <br>
+		</body>
+	  </html>
+	`
+  }
