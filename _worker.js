24f95e70c613b785499a244f848bf3ed01b7bc2e
Sinspired <ggmomo@gmail.com>
2024-04-17 23:21:36 +0800

    update cors

--- _worker.js
+++ _worker.js
@@ -1,6 +1,21 @@
 // 定义一个名为 mytoken 的变量，并将 'passwd' 作为默认的读写权限
 let mytoken = 'passwd';
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request));
+});
 
+async function handleRequest(request) {
+	const headers = new Headers({
+		'Access-Control-Allow-Origin': '*', // 替换为允许的来源，例如 'https://your-app.com'
+		'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // 替换为允许的方法
+		'Access-Control-Allow-Headers': 'Content-Type, Authorization' // 替换为允许的头部
+	});
+
+	return new Response(null, {
+		headers,
+		status: 200
+	});
+}
 
 export default {
 	async handleRequest(request, env) {
