59215a261e7b41d4d0aa98cdcabc29d9e752c139
Sinspired <ggmomo@gmail.com>
2024-04-17 20:33:57 +0800

    添加cors设置

--- _worker.js
+++ _worker.js
@@ -1,27 +1,15 @@
 // 定义一个名为 mytoken 的变量，并将 'passwd' 作为默认的读写权限
-let mytoken= 'passwd';
+let mytoken = 'passwd';
 
-// Respond to OPTIONS method
-export const onRequestOptions = async () => {
-    return new Response(null, {
-        status: 204,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Methods': 'GET, OPTIONS',
-            'Access-Control-Max-Age': '86400',
-        },
-    });
-};
 // Set CORS to all /api responses
 export const onRequest = async (context) => {
-    const response = await context.next();
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Max-Age', '86400');
-    return response;
+	const response = await context.next();
+	response.headers.set('Access-Control-Allow-Origin', '*');
+	response.headers.set('Access-Control-Max-Age', '86400');
+	return response;
 };
 export default {
-	async fetch (request, env) {
+	async fetch(request, env) {
 		// 如果环境变量中有 TOKEN，则将其赋值给 mytoken，否则保持默认值
 		mytoken = env.TOKEN || mytoken;
 
@@ -29,7 +17,7 @@ export default {
 		// 检查 KV (键值存储) 是否已经被设置
 		if (env.KV) {
 			// 将 env.KV 赋值一个名为 KV 的常量
-			KV =  env.KV;
+			KV = env.KV;
 		} else {
 			//throw new Error('KV 命名空间未绑定');
 			return new Response('KV 命名空间未绑定', {
@@ -41,7 +29,7 @@ export default {
 		// 从请求的 URL 中获取需要的参数
 		const url = new URL(request.url);
 		let token;
-		if (url.pathname === `/${mytoken}`){
+		if (url.pathname === `/${mytoken}`) {
 			token = mytoken;
 		} else {
 			// 获取 URL 查询参数中的 'token'，如果不存在则赋值为 "null"
@@ -55,23 +43,23 @@ export default {
 			if (文件名 == "config" || 文件名 == mytoken) {
 				const html = configHTML(url.hostname, token);
 				return new Response(html, {
-				  headers: {
-					'Content-Type': 'text/html; charset=UTF-8',
-				  },
+					headers: {
+						'Content-Type': 'text/html; charset=UTF-8',
+					},
 				});
 			} else if (文件名 == "config/update.bat") {
 				return new Response(下载bat(url.hostname, token), {
-				  headers: {
-					"Content-Disposition": `attachment; filename=update.bat`, 
-					"content-type": "text/plain; charset=utf-8",
-				  },
+					headers: {
+						"Content-Disposition": `attachment; filename=update.bat`,
+						"content-type": "text/plain; charset=utf-8",
+					},
 				});
 			} else if (文件名 == "config/update.sh") {
 				return new Response(下载sh(url.hostname, token), {
-				  headers: {
-					"Content-Disposition": `attachment; filename=update.sh`, 
-					"content-type": "text/plain; charset=utf-8",
-				  },
+					headers: {
+						"Content-Disposition": `attachment; filename=update.sh`,
+						"content-type": "text/plain; charset=utf-8",
+					},
 				});
 			} else {
 				// 获取 URL 查询参数中的 'text' 和 'b64'，如果不存在则赋值为 "null"
@@ -79,24 +67,24 @@ export default {
 				const b64 = url.searchParams.get('b64') || "null";
 
 				// 如果 'text' 和 'b64' 都为 "null"，则从 KV 中读取并返回文件内容
-				if (text === "null" && b64 === "null"){
+				if (text === "null" && b64 === "null") {
 					const value = await KV.get(文件名);
-					return new Response(value , {
+					return new Response(value, {
 						status: 200,
 						headers: { 'content-type': 'text/plain; charset=utf-8' },
 					});
 				} else {
 					// 检查文件是否存在
 					await fileExists(KV, 文件名);
-					
+
 					// 如果 'b64' 为 "null" ，则以明文方式写入文件，如果 'text' 为 "null" ，则以 base64 方式写入文件
-					if (b64 === "null" ){
+					if (b64 === "null") {
 						await KV.put(文件名, text);
 						return new Response(text, {
 							status: 200,
 							headers: { 'content-type': 'text/plain; charset=utf-8' },
 						});
-					} else if (text === "null" ){
+					} else if (text === "null") {
 						await KV.put(文件名, base64Decode(空格替换加号(b64)));
 						return new Response(base64Decode(空格替换加号(b64)), {
 							status: 200,
@@ -106,8 +94,8 @@ export default {
 				}
 			}
 
-			
-		} else if (url.pathname == "/"){//首页改成一个nginx伪装页
+
+		} else if (url.pathname == "/") {//首页改成一个nginx伪装页
 			return new Response(`
 			<!DOCTYPE html>
 			<html>
@@ -135,9 +123,9 @@ export default {
 			</body>
 			</html>
 			`, {
-			  headers: {
-				'Content-Type': 'text/html; charset=UTF-8',
-			  },
+				headers: {
+					'Content-Type': 'text/html; charset=UTF-8',
+				},
 			});
 		} else {// 如果 token 不符，返回 'token 有误'//
 			return new Response('token 有误', {
@@ -166,39 +154,39 @@ function 空格替换加号(str) {
 	return str;
 }
 
-function 下载bat(域名,token) {
+function 下载bat(域名, token) {
 	return [
-	  `@echo off`,
-	  `chcp 65001`,
-	  `setlocal`,
-	  ``,
-	  `set "DOMAIN=${域名}"`,
-	  `set "TOKEN=${token}"`,
-	  ``,
-	  `rem %~nx1表示第一个参数的文件名和扩展名`,
-	  `set "FILENAME=%~nx1"`,
-	  ``,
-	  `rem PowerShell命令读取文件的前65行内容，将内容转换为UTF8并进行base64编码`,
-	  `for /f "delims=" %%i in ('powershell -command "$content = ((Get-Content -Path '%cd%/%FILENAME%' -Encoding UTF8) | Select-Object -First 65) -join [Environment]::NewLine; [convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($content))"') do set "BASE64_TEXT=%%i"`,
-	  ``,
-	  `rem 将内容保存到response.txt`,
-	  `rem echo %BASE64_TEXT% > response.txt`,
-	  ``,
-	  `rem 构造带有文件名和内容作为参数的URL`,
-	  `set "URL=https://%DOMAIN%/%FILENAME%?token=%TOKEN%^&b64=%BASE64_TEXT%"`,
-	  ``,
-	  `rem 显示请求的响应 `,
-	  `rem powershell -Command "(Invoke-WebRequest -Uri '%URL%').Content"`,
-	  `start %URL%`,
-	  `endlocal`,
-	  ``,
-	  `echo 更新数据完成,倒数5秒后自动关闭窗口...`,
-	  `timeout /t 5 >nul`,
-	  `exit`
+		`@echo off`,
+		`chcp 65001`,
+		`setlocal`,
+		``,
+		`set "DOMAIN=${域名}"`,
+		`set "TOKEN=${token}"`,
+		``,
+		`rem %~nx1表示第一个参数的文件名和扩展名`,
+		`set "FILENAME=%~nx1"`,
+		``,
+		`rem PowerShell命令读取文件的前65行内容，将内容转换为UTF8并进行base64编码`,
+		`for /f "delims=" %%i in ('powershell -command "$content = ((Get-Content -Path '%cd%/%FILENAME%' -Encoding UTF8) | Select-Object -First 65) -join [Environment]::NewLine; [convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($content))"') do set "BASE64_TEXT=%%i"`,
+		``,
+		`rem 将内容保存到response.txt`,
+		`rem echo %BASE64_TEXT% > response.txt`,
+		``,
+		`rem 构造带有文件名和内容作为参数的URL`,
+		`set "URL=https://%DOMAIN%/%FILENAME%?token=%TOKEN%^&b64=%BASE64_TEXT%"`,
+		``,
+		`rem 显示请求的响应 `,
+		`rem powershell -Command "(Invoke-WebRequest -Uri '%URL%').Content"`,
+		`start %URL%`,
+		`endlocal`,
+		``,
+		`echo 更新数据完成,倒数5秒后自动关闭窗口...`,
+		`timeout /t 5 >nul`,
+		`exit`
 	].join('\r\n');
 }
 
-function 下载sh(域名,token) {
+function 下载sh(域名, token) {
 	return `#!/bin/bash
 export LANG=zh_CN.UTF-8
 DOMAIN="${域名}"
