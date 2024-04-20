1b546ac163867c84634afc276adfc2fcc4cce274
Sinspired <ggmomo@gmail.com>
2024-04-20 16:06:29 +0800

    t6

--- _worker.js
+++ _worker.js
@@ -62,15 +62,15 @@ export default {
 				// 如果 'text' 和 'b64' 都为 "null"，则从 KV 中读取并返回文件内容
 				if (text === "null" && b64 === "null") {
 					const value = await KV.get(文件名);
-					const allowedOrigin = 'https://zcrich.cn'; // 允许的域名
+					const allowedOrigin = 'https://workout.zcrich.cn'; // 允许的域名
 					const origin = request.headers.get('Origin');
 					if (origin && origin === allowedOrigin) {
-					  // 添加 CORS 头部，以允许来自允许域名的请求
-					  const response = new Response(value);
-					  response.headers.set('Access-Control-Allow-Origin', allowedOrigin);
-					  response.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-					  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Accept, Accept-Language, Accept-Encoding');
-					  return response;
+						// 添加 CORS 头部，以允许来自允许域名的请求
+						const response = new Response(value);
+						response.headers.set('Access-Control-Allow-Origin', allowedOrigin);
+						response.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+						response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Accept, Accept-Language, Accept-Encoding');
+						return response;
 					}
 					return new Response(value, {
 						status: 200,
