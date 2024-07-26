b6b67b2c6bea9ac838208d5c9b40fd48dd117148
Sinspired <63581268+sinspired@users.noreply.github.com>
2024-07-26 19:09:35 +0900

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -230,6 +230,7 @@ function configHTML(domain, token) {
             align-items: center;
             gap: 5px;
             white-space: nowrap;
+            width:100%;
         }
 
         /* Light theme */
