12f9fb927985f3fec2e187151e7cc8586334c36c
Sinspired <63581268+sinspired@users.noreply.github.com>
2024-07-26 13:43:37 +0900

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -177,8 +177,8 @@ function configHTML(domain, token) {
     <style>
         body { font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; padding: 10px; max-width: 800px; margin: 0 auto; }
         h1 { text-align: center; }
-        pre { padding: 15px; border-radius: 5px; overflow-x: auto; white-space: nowrap; }
-        pre code { background: none; padding: 0; border: none; border-radius:5px;}
+        pre { padding: 0px; border-radius: 5px; overflow-x: auto; white-space: nowrap; }
+        pre code { background: none; padding: 0; border: none; border-radius:15px;}
         button { cursor: pointer; padding: 10px 15px; margin-top: 10px; border: none; border-radius: 5px; }
         button:hover { opacity: 0.9; }
         input[type="text"] { width: calc(100% - 25px); padding: 10px; border-radius: 5px; margin-bottom: 10px; }
@@ -223,7 +223,7 @@ function configHTML(domain, token) {
         <p><strong>注意!</strong> 因URL长度内容所限，脚本更新方式一次最多更新65行内容</p>
         <h2>Windows 脚本:</h2>
         <button onclick="window.open('https://${domain}/config/update.bat?token=${token}&t=' + Date.now(), '_blank')">点击下载</button>
-        <pre><code class="language-batch">update.bat ip.txt</code></pre>
+        <pre><code class="language-dos">update.bat ip.txt</code></pre>
         <h2>Linux 脚本:</h2>
         <pre><code class="language-bash">curl "https://${domain}/config/update.sh?token=${token}&t=$(date +%s%N)" -o update.sh && chmod +x update.sh</code></pre>
         <h2>在线文档查询:</h2>
