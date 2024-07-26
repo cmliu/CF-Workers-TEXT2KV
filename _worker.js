8d3a2ab6dc4acd32879b8f2635e0103dfb30f785
Sinspired <63581268+sinspired@users.noreply.github.com>
2024-07-26 19:18:45 +0900

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -182,7 +182,7 @@ function configHTML(domain, token) {
         pre,code { padding: 0px; border-radius: 8px; overflow-x: auto; white-space: nowrap; }
         pre code { background: none; padding: 0; border: none; }
         button { 
-        width:250px;
+        width:350px;
         cursor: pointer; 
         padding: 10px 10px; 
         margin-top: 0px; 
