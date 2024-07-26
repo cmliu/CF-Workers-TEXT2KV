618d87f70b369c5195a8765c7c4e9f7943828527
Sinspired <63581268+sinspired@users.noreply.github.com>
2024-07-26 14:03:11 +0900

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -177,7 +177,7 @@ function configHTML(domain, token) {
     <style>
         body { font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; padding: 10px; max-width: 800px; margin: 0 auto; }
         h1 { text-align: center; }
-        code { background: black; padding: 0; border: none; border-radius:5px 15px;}
+        code { background: black; padding: 0 15px; border: none; border-radius:15px;}
         button { cursor: pointer; padding: 10px 15px; margin-top: 10px; border: none; border-radius: 5px; }
         button:hover { opacity: 0.9; }
         input[type="text"] { width: calc(100% - 25px); padding: 10px; border-radius: 5px; margin-bottom: 10px; }
